896b934a00a27fa31b56d51643842218f534d805
takesson <takesson@96b95f24-04cf-11e3-93fc-253e402c2b5f>
2014-03-27 15:18:11 +0000

    #scms:711 Implemented very basic XMP extraction with tika-xmp. Some basic test coverage. It is complex to traverse the XMP tree to extract all XMP metadata. Currently using a hard-coded list of XMP values of interest (can be injected in the future).

--- src/main/java/se/repos/indexing/fulltext/HandlerFulltext.java
+++ src/main/java/se/repos/indexing/fulltext/HandlerFulltext.java
@@ -7,19 +7,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.xmp.XMPMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import se.repos.indexing.IndexingDoc;
 import se.repos.indexing.IndexingItemHandler;
-import se.repos.indexing.item.IndexingItemProgress;
 import se.repos.indexing.item.HandlerPathinfo;
+import se.repos.indexing.item.IndexingItemProgress;
 import se.simonsoft.cms.item.events.change.CmsChangesetItem;
 
 public class HandlerFulltext implements IndexingItemHandler {
@@ -28,6 +30,9 @@ public class HandlerFulltext implements IndexingItemHandler {
 	 * To avoid multivalue fields for all metadata in Solr we concatenate with newline, and tokenize on that at indexing.
 	 */
 	private static final String METADATA_MULTIVALUE_SEPARATOR = "\n";
+	
+	private String[] XMP_FIELDS_DEFAULT = {"dc:description", "dc:subject", "dc:title", "cp:revision", "tiff:Orientation"};
+	private Set<String> xmpFields = new HashSet<String>(Arrays.asList(XMP_FIELDS_DEFAULT));
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
@@ -46,13 +51,12 @@ public class HandlerFulltext implements IndexingItemHandler {
 			return;
 		}
 
-		// TODO use tika-xmp instead?
+		// Indexing both traditional Metadata and XMPMetadata (tika-xmp).
 		// TODO pre-load metadata with for example explicit content type from svn prop
 		Metadata metadata = new Metadata();
-		//org.apache.tika.xmp.XMPMetadata metadata = new org.apache.tika.xmp.XMPMetadata();
 		
 		useTika(progress.getContents(), metadata, progress.getFields());
-
+			
 	}
 
 	@SuppressWarnings("serial")
@@ -81,22 +85,67 @@ public class HandlerFulltext implements IndexingItemHandler {
 		}
 		
 		indexingDoc.setField("text", text);
-		
+
+		// Extract the classic embedded metadata.
 		for (String n : metadata.names()) {
-			String fieldname = "embd_" + n.replace(' ', '_');
-			if (n.indexOf(' ') >= 0) {
-				logger.trace("Whitespace in metadata name, '{}' becomes {}", n, fieldname);
+			this.addField("embd_", n, metadata, indexingDoc);
+		}
+
+		// Extract the metadata after mapping to XMP.
+		try {
+			XMPMetadata xmpMetadata = new org.apache.tika.xmp.XMPMetadata(metadata);
+
+			// Can be used for deeper examination of XMP tree. Complex stuff.
+			/*
+			XMPIterator it = xmpMetadata.getXMPData().iterator();
+			while (it.hasNext()) { 
+				XMPPropertyInfo xmp = (XMPPropertyInfo) it.next();
+				// https://code.google.com/p/metadata-extractor/source/browse/Libraries/XMPCore/src/com/adobe/xmp/options/PropertyOptions.java
+				PropertyOptions xmpOptions = xmp.getOptions();
+				
+				System.out.println(xmp.getPath());
+				logger.trace("XMP node: {} ({}): {}", xmp.getPath(), xmpOptions.getOptionsString(), xmp.getValue());
 			}
-			if (metadata.isMultiValued(n)) {
-				StringBuffer concat = new StringBuffer();
-				for (String v : metadata.getValues(n)) {
-					concat.append(METADATA_MULTIVALUE_SEPARATOR).append(v);
+			*/
+				
+			for (String n: this.xmpFields) {
+				String[] values = xmpMetadata.getValues(n);
+				if (values != null && values[0] != null) {
+					logger.debug("XMP field {} exists.", n);
+					this.addField("xmp_", n, metadata, indexingDoc);
+				} else {
+					logger.debug("XMP field {} does NOT exist.", n);
+					//System.out.println("XMP field " + n + " does NOT exist.");
 				}
-				indexingDoc.addField(fieldname, concat.substring(1));
-			} else {
-				indexingDoc.addField(fieldname, metadata.get(n));
+				
 			}
-		}		
+			
+		} catch (Exception e) {
+			logger.warn("XMP extraction error for {}: {}", indexingDoc.getFieldValue("id"), e.getMessage());
+			StringWriter err = new StringWriter();
+			e.printStackTrace(new PrintWriter(err));
+			indexingDoc.setField("text_error", err.toString());
+			return;
+		}
 	}
 
+	private void addField(String prefix, String n, Metadata metadata, IndexingDoc indexingDoc) {
+		
+		String fieldname = prefix + n.replace(' ', '_').replace(':', '.');
+		if (n.indexOf(' ') >= 0) {
+			logger.trace("Whitespace in metadata name, '{}' becomes {}", n, fieldname);
+		}
+		if (n.indexOf(':') >= 0) {
+			logger.trace("Colon in metadata name, '{}' becomes {}", n, fieldname);
+		}
+		if (metadata.isMultiValued(n)) {
+			StringBuffer concat = new StringBuffer();
+			for (String v : metadata.getValues(n)) {
+				concat.append(METADATA_MULTIVALUE_SEPARATOR).append(v);
+			}
+			indexingDoc.addField(fieldname, concat.substring(1));
+		} else {
+			indexingDoc.addField(fieldname, metadata.get(n));
+		}
+	}
 }
--- src/test/java/se/repos/indexing/fulltext/ItemFulltextExtractionTest.java
+++ src/test/java/se/repos/indexing/fulltext/ItemFulltextExtractionTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import se.repos.indexing.IndexingDoc;
 import se.repos.indexing.item.IndexingItemProgress;
 import se.repos.indexing.item.IndexingItemStandalone;
 
@@ -15,14 +16,231 @@ import se.repos.indexing.item.IndexingItemStandalone;
  */
 public class ItemFulltextExtractionTest {
 
+	private void printFields(IndexingDoc fields) {
+
+		for (@SuppressWarnings("unused") String f : fields.getFieldNames()) {
+			//System.out.println(f);
+		}
+	}
+
 	@Test
-	public void test() {
+	public void testExcel1() {
 		HandlerFulltext fulltext = new HandlerFulltext();
-		
+
 		IndexingItemProgress item = new IndexingItemStandalone("repos-search-v1/docs/Excel format.xls");
 		fulltext.handle(item);
+		IndexingDoc fields = item.getFields();
+
+		assertTrue("Should extract text", fields.containsKey("text"));
+
+		printFields(fields);
+		/*
+		 * With generic Metadata:
+		text
+		embd_Revision-Number
+		embd_cp:revision
+		embd_Last-Printed
+		embd_meta:print-date
+		embd_meta:creation-date
+		embd_dcterms:modified
+		embd_meta:save-date
+		embd_dcterms:created
+		embd_Last-Modified
+		embd_date
+		embd_modified
+		embd_Edit-Time
+		embd_Creation-Date
+		embd_Content-Type
+		embd_Last-Save-Date
+		 * 
+		 */
+		assertEquals("CP Revision", "1", fields.getFieldValue("xmp_cp.revision"));
+	}
+
+	@Test
+	public void testWord1() {
+		HandlerFulltext fulltext = new HandlerFulltext();
+
+		IndexingItemProgress item = new IndexingItemStandalone("repos-search-v1/docs/Word document.doc");
+		fulltext.handle(item);
+		IndexingDoc fields = item.getFields();
+
+		assertTrue("Should extract text", fields.containsKey("text"));
+
+		printFields(fields);
+		/*
+		 * With generic Metadata:
+		text
+		embd_Revision-Number
+		embd_cp:revision
+		embd_Last-Printed
+		embd_meta:print-date
+		embd_meta:creation-date
+		embd_dcterms:modified
+		embd_meta:save-date
+		embd_dcterms:created
+		embd_Last-Modified
+		embd_date
+		embd_modified
+		embd_Edit-Time
+		embd_Creation-Date
+		embd_Content-Type
+		embd_Last-Save-Date
+		 * 
+		 */
+		//assertEquals("DC Format", "something", fields.getFieldValue("xmp_dc.format"));
+		assertEquals("CP Revision", "1", fields.getFieldValue("xmp_cp.revision"));
+	}
+
+	@Test
+	public void testPdf1() {
+		HandlerFulltext fulltext = new HandlerFulltext();
+
+		IndexingItemProgress item = new IndexingItemStandalone("repos-search-v1/docs/svnprops/shortpdf.pdf");
+		fulltext.handle(item);
+		IndexingDoc fields = item.getFields();
+
+		assertTrue("Should extract text", fields.containsKey("text"));
+
+		printFields(fields);
+
+		assertEquals("Subject", "keywordinsaveaspdf someotherkeyword", fields.getFieldValue("xmp_dc.subject"));
+		assertEquals("Title", "PDF Title For Short Document", fields.getFieldValue("xmp_dc.title"));
+		/*
+		 * With generic Metadata:
+		text
+		embd_dc:subject
+		embd_meta:save-date
+		embd_subject
+		embd_Author
+		embd_dcterms:created
+		embd_date
+		embd_creator
+		embd_Creation-Date
+		embd_title
+		embd_meta:author
+		embd_created
+		embd_meta:keyword
+		embd_cp:subject
+		embd_xmp:CreatorTool
+		embd_Keywords
+		embd_dc:title
+		embd_Last-Save-Date
+		embd_meta:creation-date
+		embd_dcterms:modified
+		embd_dc:creator
+		embd_Last-Modified
+		embd_AAPL:Keywords
+		embd_modified
+		embd_xmpTPg:NPages
+		embd_producer
+		embd_Content-Type
+		 */
+	}
+
+	@Test
+	public void testJpgCommentXnview1() {
+		HandlerFulltext fulltext = new HandlerFulltext();
+
+		IndexingItemProgress item = new IndexingItemStandalone("repos-search-v1/docs/images/testJPEG_commented_xnviewmp026.jpg");
+		fulltext.handle(item);
+		IndexingDoc fields = item.getFields();
+
+		// Should we add the text field even if it is empty?
+		assertTrue("Should extract text", fields.containsKey("text"));
+		assertEquals("Text", "", fields.getFieldValue("text"));
 		
-		assertTrue("Should extract text", item.getFields().containsKey("text"));
+		printFields(fields);
+
+
+		// For this image, the XMP extraction finds namespaces: dc (2), tiff (10), exif (just 7) 
+		assertEquals("Subject (keywords)", "bird watching\ncoast\nnature reserve\ngrazelands", fields.getFieldValue("xmp_dc.subject"));
+		assertEquals("Description", "Bird site in north eastern Skåne, Sweden.\n(new line)", fields.getFieldValue("xmp_dc.description"));
+		assertEquals("1", fields.getFieldValue("xmp_tiff.Orientation"));
+		
+		/*
+		 * From generic Metadata:
+		text
+		embd_GPS_Altitude_Ref
+		embd_Model
+		embd_Exif_Version
+		embd_Exposure_Mode
+		embd_dc.description
+		embd_tiff.ImageLength
+		embd_Creation-Date
+		embd_exif.Flash
+		embd_ISO_Speed_Ratings
+		embd_X_Resolution
+		embd_Shutter_Speed_Value
+		embd_description
+		embd_tiff.ImageWidth
+		embd_Jpeg_Comment
+		embd_tiff.XResolution
+		embd_Image_Width
+		embd_Keywords
+		embd_GPS_Longitude
+		embd_GPS_Longitude_Ref
+		embd_exif.FNumber
+		embd_GPS_Altitude
+		embd_F-Number
+		embd_Comments
+		embd_Color_Space
+		embd_meta.creation-date
+		embd_Resolution_Units
+		embd_Data_Precision
+		embd_tiff.BitsPerSample
+		embd_tiff.YResolution
+		embd_YCbCr_Positioning
+		embd_Compression_Type
+		embd_Components_Configuration
+		embd_exif.IsoSpeedRatings
+		embd_Thumbnail_Offset
+		embd_Exif_Image_Height
+		embd_Focal_Length
+		embd_Thumbnail_Length
+		embd_Thumbnail_Compression
+		embd_comment
+		embd_Content-Type
+		embd_White_Balance_Mode
+		embd_tiff.Orientation
+		embd_Make
+		embd_tiff.Make
+		embd_Date/Time_Original
+		embd_dc.subject
+		embd_subject
+		embd_Exif_Image_Width
+		embd_Scene_Capture_Type
+		embd_dcterms.created
+		embd_exif.ExposureTime
+		embd_Component_1
+		embd_GPS_Latitude
+		embd_Component_2
+		embd_Component_3
+		embd_GPS_Latitude_Ref
+		embd_tiff.ResolutionUnit
+		embd_Flash
+		embd_Date/Time_Digitized
+		embd_meta.keyword
+		embd_Resolution_Unit
+		embd_White_Balance
+		embd_Number_of_Components
+		embd_Aperture_Value
+		embd_tiff.Model
+		embd_Orientation
+		embd_Image_Height
+		embd_w.comments
+		embd_geo.lat
+		embd_Exposure_Time
+		embd_exif.DateTimeOriginal
+		embd_exif.FocalLength
+		embd_FlashPix_Version
+		embd_Custom_Rendered
+		embd_geo.long
+		embd_Digital_Zoom_Ratio
+		embd_GPS_Version_ID
+		embd_Gain_Control
+		embd_Y_Resolution
+		 */
 	}
 
 }
